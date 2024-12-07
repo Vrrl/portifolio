e5b68c30da7a15eeca734a7b223f1d410a8ac497
Gabriel Verle <nykogabriel@gmail.com>
2024-12-07 18:11:59 -0300

    Changed config

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,10 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  assetPrefix: isProd ? '/portifolio/' : '',
-  basePath: isProd ? '/portifolio' : '',
+  // assetPrefix: isProd ? '/portifolio/' : '',
+  // basePath: isProd ? '/portifolio' : '',
   reactStrictMode: true,
+  trailingSlash: true,
   distDir: 'out',
 };
 
