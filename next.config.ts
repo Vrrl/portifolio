4c6e2d0cefc80b314a5f2a88a2c007ef18f39fc0
Gabriel Verle <nykogabriel@gmail.com>
2024-12-07 19:11:26 -0300

    Test image

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   distDir: 'out',
+  images: {
+    unoptimized: true, // Desabilitar otimização automática de imagens
+    loader: 'imgix', // Usar loader imgix para o caminho correto
+    path: '/portifolio/', // Definir caminho base para imagens
+  },
 };
 
 export default nextConfig;
